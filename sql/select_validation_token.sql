98ad31a4ecc81eeca3198c923fd4722664cc08f6
Trevor Linton <trevor.linton@gmail.com>
2018-02-15 17:16:38 -0700

    COBRA-1610b - fix for auto validations

--- sql/select_validation_token.sql
+++ sql/select_validation_token.sql
@@ -1,10 +1,20 @@
 select 
-  auto_builds.auto_build, auto_builds.app, auto_builds.repo, auto_builds.branch, auto_builds.auto_deploy, auto_builds.validation_token,
-  authorizations.user_id, authorizations.username, authorizations.token,
-  organizations.name org, apps.name appname, spaces.name spacename
+  auto_builds.auto_build, 
+  auto_builds.app, 
+  auto_builds.repo, 
+  auto_builds.branch, 
+  case when features.feature is null then false else true end auto_deploy,
+  auto_builds.validation_token,
+  authorizations.user_id, 
+  authorizations.username, 
+  authorizations.token,
+  organizations.name org, 
+  apps.name appname, 
+  spaces.name spacename
 from
   auto_builds
     join apps on auto_builds.app = apps.app
+    left join features on apps.app = features.app and features.name = 'auto-release' and features.deleted = false
     join authorizations on  auto_builds.authorization = authorizations.authorization
     join spaces on apps.space = spaces.space 
     join organizations on apps.org = organizations.org

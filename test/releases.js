4dd8600d7c5ddf5324f8b60dc4f7609256245246
Murray Resinski <murray.resinski@octanner.com>
2018-04-05 10:58:52 -0600

    added check of page content after putting up maintenance page

--- test/releases.js
+++ test/releases.js
@@ -191,6 +191,7 @@ describe("releases: list, get, create a release", function() {
           let maint_info_on = JSON.parse(await httph.request('patch', `http://localhost:5000/apps/${appname_brand_new}-default`, alamo_headers, JSON.stringify(payload)))
           expect(maint_info_on.maintenance).to.equal(true)
           let actual_page = await httph.request('get', `${maint_info_on.web_url}`, null)
+          expect(actual_page).to.contain("System Maintenance")
           payload.maintenance = false
           let maint_info_off = JSON.parse(await httph.request('patch', `http://localhost:5000/apps/${appname_brand_new}-default`, alamo_headers, JSON.stringify(payload)))
           expect(maint_info_off.maintenance).to.equal(false)

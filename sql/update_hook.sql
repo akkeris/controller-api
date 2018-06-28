19de20e937e8cc870046cc1163b3d98f52c78461
Trevor Linton <trevor.linton@gmail.com>
2018-06-28 10:28:48 -0600

    fixed second test issue, also added tests

--- sql/update_hook.sql
+++ sql/update_hook.sql
@@ -2,7 +2,7 @@ update hooks set
   events = coalesce($2, events),
   url = coalesce($3, url),
   secret = coalesce($4, secret),
-  active = coalesce($4, active),
+  active = coalesce($5, active),
   updated = now()
 where
   hook::varchar(128) = $1::varchar(256) and

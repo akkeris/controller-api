53c93dd6306e1ad0e906fbce616d92a7f28b9cbc
swathirajashekar <swathi.rajashekar@octanner.com>
2018-11-01 13:14:39 -0600

    Fix for creating duplicate subscription to return OK with same ACL (#118)

--- sql/insert_topic_acl.sql
+++ sql/insert_topic_acl.sql
@@ -2,4 +2,6 @@ insert into topic_acls
   (topic_acl, topic, app, role, consumer_group_name)
 values
   ($1, $2, $3, $4, $5)
+on conflict on constraint topic_acls_pkey do 
+  update set consumer_group_name = topic_acls.consumer_group_name 
 returning *

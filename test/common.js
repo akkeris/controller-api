8fc93d33b6de406fc129ca56535ca39283c9e28c
Trevor Linton <trevor.linton@gmail.com>
2019-02-13 09:34:18 -0700

    COBRA-2177 - fixed redaction and added addition tests

--- test/common.js
+++ test/common.js
@@ -64,5 +64,69 @@ describe("base: Ensure helper tools work correctly.", () => {
       expect(JSON.parse(data)).to.be.an('array')
       done();
     });
-  })
+  });
+  it("Ensure we redact passwords in uri's.", (done) => {
+    let res = common.socs({"SOME_URI":"https://foobar:fee@host.com"});
+    expect(res.SOME_URI).to.equal("https://foobar:[redacted]@host.com");
+    done();
+  });
+  it("Ensure we redact passwords in uri's with ports.", (done) => {
+    let res = common.socs({"SOME_URI":"https://foobar:fee@host.com:1234"});
+    expect(res.SOME_URI).to.equal("https://foobar:[redacted]@host.com:1234");
+    done();
+  });
+  it("Ensure we redact passwords in uri's with paths no ports.", (done) => {
+    let res = common.socs({"SOME_URI":"https://foobar:fee@host.com/foo"});
+    expect(res.SOME_URI).to.equal("https://foobar:[redacted]@host.com/foo");
+    done();
+  });
+  it("Ensure we redact passwords in uri's with paths no ports and args.", (done) => {
+    let res = common.socs({"SOME_URI":"https://foobar:fee@host.com/foo?foo=bar"});
+    expect(res.SOME_URI).to.equal("https://foobar:[redacted]@host.com/foo?foo=bar");
+    done();
+  });
+  it("Ensure we redact passwords in uri's where it may appear as a query param.", (done) => {
+    let res = common.socs({"SOME_URI":"https://foobar:fee@host.com/foo?password=foobar"});
+    expect(res.SOME_URI).to.equal("https://foobar:[redacted]@host.com/foo?password=[redacted]");
+    done();
+  });
+  it("Ensure we redact passwords in uri's where it may appear as a query param with multiple args, first.", (done) => {
+    let res = common.socs({"SOME_URI":"https://foobar:fee@host.com/foo?password=foobar&justfine=true"});
+    expect(res.SOME_URI).to.equal("https://foobar:[redacted]@host.com/foo?password=[redacted]&justfine=true");
+    done();
+  });
+  it("Ensure we redact passwords in uri's where it may appear as a query param with multiple args, not-first.", (done) => {
+    let res = common.socs({"SOME_URI":"https://foobar:fee@host.com/foo?justfine=true&password=foobar"});
+    expect(res.SOME_URI).to.equal("https://foobar:[redacted]@host.com/foo?justfine=true&password=[redacted]");
+    done();
+  });
+  it("Ensure we redact passwords in uri's where it may appear as a query param with multiple args, middle.", (done) => {
+    let res = common.socs({"SOME_URI":"https://foobar:fee@host.com/foo?justfine=true&password=foobar&alsofine=true"});
+    expect(res.SOME_URI).to.equal("https://foobar:[redacted]@host.com/foo?justfine=true&password=[redacted]&alsofine=true");
+    done();
+  });
+  it("Ensure we redact passwords in uri's with a wide variety of characters.", (done) => {
+    let res = common.socs({"SOME_URI":"https://foobar:1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!#$%^&*()_+-={}|[]\\:\";'<>?,./@host.com/path?arg=arg3224&foo=bar"});
+    expect(res.SOME_URI).to.equal("https://foobar:[redacted]@host.com/path?arg=arg3224&foo=bar");
+    done();
+  });
+  it("Ensure we redact passwords in uri's with a wide variety of characters (with a port).", (done) => {
+    let res = common.socs({"SOME_URI":"https://foobar:1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!#$%^&*()_+-={}|[]\\:\";'<>?,./@host.com:1235/path?arg=arg3224&foo=bar"});
+    expect(res.SOME_URI).to.equal("https://foobar:[redacted]@host.com:1235/path?arg=arg3224&foo=bar");
+    done();
+  });
+  it("Ensure we redact passwords in uri's with a wide variety of characters (without a username).", (done) => {
+    let res = common.socs({"SOME_URI":"https://:1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!#$%^&*()_+-={}|[]\\:\";'<>?,./@host.com/path?arg=arg3224&foo=bar"});
+    expect(res.SOME_URI).to.equal("https://:[redacted]@host.com/path?arg=arg3224&foo=bar");
+    done();
+  });
+  it("Ensure we redact passwords where the key has a potentially sensitive name.", (done) => {
+    let res = common.socs({"SOME_URI":"valid", "SOME_TOKEN":"should be redacted", "SOME_KEY":"should be also redacted", "SOME_PASS":"should be redacted?", "SOME_PASSWORD":"should be redacted", "SOME_SECRET":"should be redacted"});
+    expect(res.SOME_URI).to.equal("valid");
+    expect(res.SOME_TOKEN).to.equal("[redacted]");
+    expect(res.SOME_PASS).to.equal("[redacted]");
+    expect(res.SOME_PASSWORD).to.equal("[redacted]");
+    expect(res.SOME_SECRET).to.equal("[redacted]");
+    done();
+  });
 });

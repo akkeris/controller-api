ac4a54b4a414fad5cf81d35f5c3ecaae466a6401
Trevor Linton <trevor.linton@gmail.com>
2019-06-27 11:38:57 -0600

    COBRA-2507 - added sql and missing test

---
+++ sql/select_filter_attachments.sql
@@ -0,0 +1,16 @@
+select 
+	filter_attachments.filter_attachment, 
+	filter_attachments.filter,
+	filter_attachments.app,
+	filter_attachments.attachment_options,
+	filter_attachments.deleted,
+	filter_attachments.created_by,
+	filter_attachments.created,
+	filter_attachments.updated,
+	filter_attachments.deleted
+from
+	filter_attachments
+		join filters on filter_attachments.filter = filters.filter and filters.deleted = false
+		join apps on filter_attachments.app = apps.app and apps.deleted = false
+where
+	filter_attachments.app = $1 and filter_attachments.deleted = false

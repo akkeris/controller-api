fc0f4540f19a4850aee5f623e356fb9c93fec50c
Trevor Linton <trevor.linton@gmail.com>
2019-11-01 14:52:25 -0600

    COBRA-1979g - additional fixes

--- sql/insert_release_status.sql
+++
@@ -1,2 +0,0 @@
-insert into release_statuses (release_status, release, state, name, context, description, target_url, image_url, created, updated, deleted) 
-values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,false)
--- sql/update_release_status.sql
+++ sql/update_release_status.sql
@@ -1,11 +1,1 @@
-update release_statuses set
-  name = coalesce($3, name),
-  image_url = coalesce($4, image_url),
-  target_url = coalesce($5, target_url),
-  description = coalesce($6, description),
-  state = coalesce($7, state)
-  updated = now()
-where
-  (release_statuses.release_status::varchar(1024) = $1::varchar(1024) OR release_statuses.context::varchar(1024) = $1::varchar(1024)) and
-  release_statuses.release = $2
-  release_statuses.deleted = false
+update releases set status = $3 where app = $1 and release = $2

a126f6d30154ca028a7bd746e4620a920b1d94ee
Sam Beckett <sbeck14@me.com>
2021-09-02 16:26:08 -0600

    set rec action to unique string upon deletion

--- sql/delete_recommendation_by_id.sql
+++ sql/delete_recommendation_by_id.sql
@@ -1,6 +1,9 @@
+-- Need to set action to a unique string
+-- In this case, "[action]_deleted_[timestamp]_[random_chars]"
 update recommendations set
     deleted = true,
-    updated = now()
+    updated = now(),
+    action = recommendations.action || '_deleted_' || extract(epoch from now()) || '_' || substr(md5(random()::text), 0, 8)
 where
     recommendation = $1 and
     deleted = false

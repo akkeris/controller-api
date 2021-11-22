ac5e4c828f6db7614bfdc6aad4a2492461542dbd
Sam Beckett <sbeck14@me.com>
2021-11-22 14:58:05 -0700

    add simple queryparam for GET /apps

---
+++ sql/select_apps_simple.sql
@@ -0,0 +1,27 @@
+select
+  apps.app app_uuid,
+  apps.created,
+  apps.updated,
+  apps.name app_name,
+  apps.disabled,
+  apps.description description,
+  apps.labels labels,
+  spaces.name space_name,
+  spaces.space space_uuid,
+  stacks.stack stack_uuid,
+  stacks.name stack_name,
+  regions.region region_uuid,
+  regions.name region_name,
+  organizations.name org_name,
+  organizations.org org_uuid,
+  (select preview from previews where apps.app = previews.target and previews.deleted = false limit 1) preview
+from
+  apps
+    join spaces on apps.space = spaces.space
+    join organizations on apps.org = organizations.org
+    join stacks on spaces.stack = stacks.stack and stacks.deleted = false
+    join regions on regions.region = stacks.region and regions.deleted = false
+where
+  apps.deleted = false and
+  spaces.deleted = false
+order by apps.name, spaces.name desc
